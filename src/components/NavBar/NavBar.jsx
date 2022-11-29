cd50a6fe15e0e2dca192c4345758554f5c823d2a
kangcarol <otherhats@gmail.com>
2022-11-28 22:03:24 -0500

    nav links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,18 @@
 import styles from './NavBar.module.css'
-
+import { Link, useLocation } from 'react-router-dom'
 
 function NavBar() {
+  const location = useLocation()
+
   return (
-    <main className={styles.NavBar}>
-      <h1>NavBar</h1>
-    </main>
+    <nav className={styles.NavBar}>
+      <ul>
+        {(location.pathname ==='/') ? '' : <li><Link to='/'>CAROL KANG</Link></li>}
+        <li><Link to='/about'>ABOUT</Link></li>
+        <li><Link to='/collections'>COLLECTIONS</Link></li> 
+        <li><Link to='/contact'>CONTACT</Link></li>
+      </ul>
+    </nav>
   )
 }
 
