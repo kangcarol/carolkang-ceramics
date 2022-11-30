ecc3814acaa8d1841249c788a345aff3265dc9cc
kangcarol <otherhats@gmail.com>
2022-11-29 22:56:14 -0500

    about text draft

---
+++ src/data/collections.js
@@ -0,0 +1,29 @@
+export const collections = [
+  {
+    title: "vessels",
+    description: "",
+    image1: '',
+    image2: '',
+    image3: '',
+    image4: '',
+    image5: '',
+  },
+  {
+    title: "vessels",
+    description: "",
+    image1: '',
+    image2: '',
+    image3: '',
+    image4: '',
+    image5: '',
+  },
+  {
+    title: "tableware",
+    description: "",
+    image1: '',
+    image2: '',
+    image3: '',
+    image4: '',
+    image5: '',
+  },
+]
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,19 @@ import styles from './About.module.css'
 function About() {
   return (
     <main className={styles.about}>
-      <h1>About</h1>
+        <section>
+          <img 
+            src='./carolkang.jpeg'
+            alt="carol-kang"
+            className="self-img" 
+          />
+          <p>Movement and fluidity is something I think about when creating with clay, whether it is sculpture or functional. I welcome the responsiveness of the clay, which inspires new evolutions in my work</p>
+          
+          <p>For sculpture and decorative vessels, I work primarily on the wheel, often throwing pieces and combining them into one seamless work.</p>
+          
+          <p>The tableware I make is intended to be interlaced with daily living, becoming reflections of our lives.</p>
+
+      </section>
     </main>
   )
 }
